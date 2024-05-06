1363e428864b3f2b5c45d9808d55effde00b2ab4
iamcult <101368650+iamcult@users.noreply.github.com>
2024-05-06 19:48:08 -0400

    Version 38

--- home.nix
+++ home.nix
@@ -27,7 +27,7 @@
       set fish_greeting # Disable greeting
       alias nixos-version="nix profile history --profile /nix/var/nix/profiles/system | tail -2 | grep Version | rev | cut -c 20- | rev"
       alias commit-os="pushd ~/nixos; nixos-version | ansi2txt | git commit -a -F - ; popd"
-      alias rebuild-os="doas nixos-rebuild switch --flake ~/nixos#thing && commit-os"
+      alias rebuild-os="nix flake update ~/nixos#thing && doas nixos-rebuild switch --flake ~/nixos#thing && commit-os"
       alias push-os="pushd ~/nixos; git push; popd"
       alias clear="clear && pfetch"
       pfetch
